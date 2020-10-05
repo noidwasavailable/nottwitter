aedfb7ef1e9bfd8cdd12d440bd2d6457a7f9fdd9
noidwasavailable <choi9601@gmail.com>
2020-10-05 16:53:10 +0900

    added .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
-import * as firebase from "firebase";
+import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyBWGpRDdmIDhdvgokhy-wsFOGzLsIsdQfw",
-	authDomain: "nottwitternomad.firebaseapp.com",
-	databaseURL: "https://nottwitternomad.firebaseio.com",
-	projectId: "nottwitternomad",
-	storageBucket: "nottwitternomad.appspot.com",
-	messagingSenderId: "532949917568",
-	appId: "1:532949917568:web:b71f618aa56334a0c7eb65",
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_DB_URL,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
