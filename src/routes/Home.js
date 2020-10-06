5c6df7d92e8c83790b0bfd9693d3896aa9475fdd
noidwasavailable <choi9601@gmail.com>
2020-10-06 15:53:38 +0900

    set up basic tweet form and db

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+	const [tweet, setTweet] = useState("");
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+	};
+
+	const onChange = (event) => {
+		const {
+			target: { value },
+		} = event;
+
+		setTweet(value);
+	};
+
+	return (
+		<div>
+			<div>Home</div>
+			<form onSubmit={onSubmit}>
+				<input
+					type="text"
+					value={tweet}
+					onChange={onChange}
+					placeholder="What's on your mind?"
+					maxLength={140}
+				/>
+				<input type="submit" value="NotTweet"></input>
+			</form>
+		</div>
+	);
+};
 export default Home;
